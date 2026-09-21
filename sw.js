38ecdece61203161f1b45ad63a1f3bf01f1613dd
197Edson <edsondunga27@gmail.com>
2026-09-21 17:06:39 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+﻿const CACHE_NAME = 'doefarmapet-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.php',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Instalação do Service Worker e gravação do cache inicial
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepção de requisições (carrega do cache se estiver offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
